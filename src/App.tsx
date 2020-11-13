e168e6888adec856b9e25d3f5aedbd7033141516
Lee-Seonghyun316 <mn031621019@gmail.com>
2020-11-13 17:41:38 +0900

    login3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React, {useEffect, useState} from 'react';
-//import UserList from "./UserList";
-//import SignUp from "./SignUp";
-//import Counter from "./Counter";
-//import Hello from "./Hello";
-//import PasswordConfirm from "./passwordConfirm";
-import LoginComplement from "./LoginComplement";
-import MyForm from './MyForm';
 
+
+import MyForm from './MyForm';
+//import LoginComplement from "./LoginComplement";
 
 const App: React.FC = () => {
     const onSubmit = (form: { name: string; description: string; password: boolean}) => {
@@ -26,17 +22,6 @@ const App: React.FC = () => {
         document.write(form.name + '님 환영합니다.')
         <LoginComplement />
 
-        const[passwordOK, setPasswordOK] = useState(false);
-        useEffect( () => {
-            if(form.description == "1234" && form.name !=="") {
-                setPasswordOK(true);
-            }
-            else {
-                setPasswordOK(false);
-            }
-
-
-        }, [form.description]);
         */
 
     };
@@ -45,33 +30,4 @@ const App: React.FC = () => {
     return <MyForm onSubmit={onSubmit} />;
 };
 
-/*
-function App() {
-
-    const[toggle, setToggle] = useState(true);
-    const ToggleModeChange = () =>setToggle(prev=>!prev);
-
-
-    const [visible, setVisible] = useState(true);
-
-    return (
-        //<PasswordConfirm />
-         //<Hello name="이성현" color="red" />
-          //<Counter />
-        // <SignUp />
-        <div>
-            <button
-                onClick={() => {
-                    setVisible(!visible);
-                }}
-            >
-                {visible ? '로그인' : '로그아웃'}
-            </button>
-            <hr />
-
-            {visible ? <UserList/> : <div>이성현님 환영합니다. </div>}
-        </div>
-    );
-}
-*/
 export default App;
--- src/LoginComplement.tsx
+++ src/LoginComplement.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import MyForm from './MyForm';
 
-const LoginComplement=()=> {
+const LoginComplement = () =>{
 
-    return(
+
+    return (
         <div>
-            <h1>환영합니다. </h1>
+
+            <p style={{color:"red"}}>님 환영합니다. </p>
+
+
 
         </div>
-    );
 
+    );
 };
 
 export default LoginComplement;
